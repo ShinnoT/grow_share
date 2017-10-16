0b8e843f9d5ed7238ede731cab2ed4b68922c028
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-10-17 07:41:05 +0900

    added associations for models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
-  # belongs_to :user
-  # belongs_to :item
-  has_many :users
-  has_many :items
+  belongs_to :user
+  belongs_to :item
+  # has_many :users
+  # has_many :items
 end
