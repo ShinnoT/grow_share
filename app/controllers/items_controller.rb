e9d942331c99c967d3ed484df713d883092747f6
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-10-17 16:32:10 +0900

    completed new and create action for items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,11 +10,26 @@ class ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.user = current_user
+    if @item.save
+      redirect_to item_path(@item)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :good_until, :category, :quantity, :indicator)
   end
 end
