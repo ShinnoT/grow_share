f5c94edbb59fcb926b0fd2781acabb7980d3467a
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-10-17 14:06:33 +0900

    created routes for models. index, show, new, create

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   # devise_for :users
   # root to: 'items#index'
   # resources :items, only: [:show, :new, :create, :edit, :update, :destroy] do
   #   resources :bookings, only: [:new, :create]
   # end
-=======
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  root to: 'pages#home'
->>>>>>> master
+  root to: 'items#index'
+  resources :items, only: [:show, :new, :create] do
+    resources :bookings, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
