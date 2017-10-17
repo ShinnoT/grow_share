84ae3902912f319569f7ebe08b76c7317811061c
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-10-17 13:03:42 +0900

    worked on routes, still incomplete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,10 @@
 class ItemsController < ApplicationController
+  def index
+    @items = if params[:something]
+      Item.find_by(name: params[:something])
+    else
+      Item.all
+    end
+  end
+
 end
