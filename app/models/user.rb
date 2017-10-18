85705c801f7086300b8c95821724a7132d777aa9
Neema Zandi <neemazandi@gmail.com>
2017-10-18 15:09:19 +0900

    added map, gems, fixed search, fixed index style--worked on controller for map but still not functional

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:facebook]
 
