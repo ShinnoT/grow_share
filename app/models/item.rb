ddffd89e0fe65c2ff2d74ff736e36ed08556c348
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-10-19 13:49:59 +0900

    edited price and description attribute type

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_many :bookings
   validates :name, :good_until, :category, :quantity, :indicator, :description, :price,  presence: true
-  validates :quantity, numericality: { only_integer: true }
+  validates :quantity, :price, numericality: { only_integer: true }
   validates :category, inclusion: { in: ["vegetable", "fruit"], allow_nil: false }
   validates :indicator, inclusion: { in: ["kg", "g"], allow_nil: false }
   def self.search(query)
